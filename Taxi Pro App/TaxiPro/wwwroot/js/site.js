65a65c2e6abade1484630c5c25b1446aa081ab97
Lissa Goforth <lissagoforth@gmail.com>
2018-12-15 13:36:43 -0600

    added id attribute to to video tag, cleaned up js

--- Taxi Pro App/TaxiPro/wwwroot/js/site.js
+++ Taxi Pro App/TaxiPro/wwwroot/js/site.js
@@ -3,19 +3,22 @@ var checkboxes = $("input[type='checkbox']"),
     button = $("a.IU.btn.btn-default");
 
 checkboxes.click(function () {
-    button.attr("disabled", !checkboxes.is(":checked"))
-    button.addClass("button")
+    button.attr("disabled", !checkboxes.is(":checked"));
+    button.addClass("button");
     button.removeClass;
 });
 
 
-var videos = ["vid-1", "vid-2", "vid-3", "vid-4", "vid-5"]
-var i = 0
-var visvId = videos[i];
-vid = document.getElementById(visvId);
-if (!vid) {
-    vid.style.display = "block";
+function prepVideos() {
+    var videos = ["vid-1", "vid-2", "vid-3", "vid-4", "vid-5"];
+    var i = 0;
+    var visvId = videos[i];
+    vid = document.getElementById(visvId);
+    if (!vid) {
+        vid.style.display = "block";
+    }
 }
+
 function nextVideo(divId) {
     if (visvId === divId) {
         vid.style.display = "block";
@@ -23,7 +26,7 @@ function nextVideo(divId) {
         vid.style.display = "none";
     }
     
-    i ++
+    i++;
     }
 
 function inputCheck() {
